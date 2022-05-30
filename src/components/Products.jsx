93d3e634a9016764469c97682d1ae52e7ac67551
Kundan Kumar Keshri <101567147+kundan799@users.noreply.github.com>
2022-05-30 17:02:15 +0530

    added

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,16 +1,33 @@
 import React from "react";
+import AddProduct from "./AddProduct";
+import Pagination from "./Pagination";
+import Product from "./Product"
+import { Flex, Grid, Spacer } from '@chakra-ui/react'
+
 
 const Products = () => {
   // TODO: Remove below const and instead import them from chakra
-  const Flex = () => <div />;
-  const Grid = () => <div />;
+  // const Flex = () => <div />;
+  // const Grid = () => <div />;
+ 
+
 
   return (
-    <Flex>
+    <>
+    <Flex border='1px solid' height="300px">
       {/*  AddProduct */}
-      <Grid>{/* List of Products */}</Grid>
+    <AddProduct/>
+ 
+      <Grid>
+        {/* List of Products */}
+        <Product/>
+
+      </Grid>
       {/* Pagination */}
+     <Pagination/>
+
     </Flex>
+    </>
   );
 };
 
